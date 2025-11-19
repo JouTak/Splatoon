9a3229ff1d9e962077a45fdadda7875e73dec201
acrighthere <rodionovmaksprog@mail.ru>
2025-11-20 02:34:24 +0300

    Изменена логика распределения по командам

--- src/main/kotlin/ru/joutak/splatoon/scripts/GameManager.kt
+++ src/main/kotlin/ru/joutak/splatoon/scripts/GameManager.kt
@@ -8,8 +8,7 @@ import org.bukkit.Difficulty
 import org.bukkit.GameMode
 import org.bukkit.GameRule
 import org.bukkit.World
-import ru.joutak.minigames.domain.GameQueue
-import ru.joutak.minigames.util.TeamBalancer
+import ru.joutak.minigames.domain.GameInstance
 import ru.joutak.splatoon.SplatoonPlugin
 import java.io.File
 import java.util.UUID
@@ -17,10 +16,18 @@ import java.util.UUID
 object GameManager {
     val playerGame = mutableMapOf<UUID, Game>()
     val arenas: MutableMap<String, World> = mutableMapOf()
-    fun createGame() {
-        val multiverseCore: MultiverseCore =
-            Bukkit.getServer().pluginManager.getPlugin("Multiverse-Core") as MultiverseCore
-        val template = Bukkit.getWorld(SplatoonPlugin.instance.mapName)
+
+    fun createGame(instance: GameInstance) {
+        val multiverseCore =
+            Bukkit.getPluginManager().getPlugin("Multiverse-Core") as MultiverseCore
+
+        // world template from instance config meta
+        val templateWorldName = instance.config.meta["world"] as? String
+            ?: SplatoonPlugin.instance.mapName
+
+        val template = Bukkit.getWorld(templateWorldName)
+            ?: throw IllegalStateException("Template world $templateWorldName not found")
+
         val mvWorld = multiverseCore.mvWorldManager.getMVWorld(template)
         mvWorld.setTime("day")
         mvWorld.setEnableWeather(false)
@@ -31,41 +38,46 @@ object GameManager {
         mvWorld.setAllowAnimalSpawn(false)
         mvWorld.setAllowMonsterSpawn(false)
         mvWorld.allowPortalMaking(AllowedPortalType.NONE)
+
         var key = 1
-        while (arenas.containsKey("${SplatoonPlugin.instance.mapName}_${key}")) {
-            key++
-        }
-        val worldName = "${SplatoonPlugin.instance.mapName}_${key}"
-        multiverseCore.mvWorldManager.cloneWorld(template!!.name, worldName)
-        val world = getWorld(worldName)!!
+        while (arenas.containsKey("${templateWorldName}_$key")) key++
+
+        val worldName = "${templateWorldName}_$key"
+        multiverseCore.mvWorldManager.cloneWorld(template.name, worldName)
+
+        val world = getWorld(worldName)
+            ?: throw IllegalStateException("World clone $worldName failed")
+
         world.setGameRule(GameRule.FALL_DAMAGE, false)
         world.setGameRule(GameRule.DROWNING_DAMAGE, false)
         world.setGameRule(GameRule.FIRE_DAMAGE, false)
-        arenas[worldName] = Bukkit.getWorld(worldName)!!
+
+        arenas[worldName] = world
+
         val game = Game(worldName)
 
-        val teams = TeamBalancer.distributePlayers(GameQueue.getQueue(), 4)
-
-        teams.forEach { team ->
-            var commandNum = 0
-            team.members.forEach { player ->
-                playerGame[Bukkit.getPlayer(player.name)!!.uniqueId] = game
-                game.commands += Bukkit.getPlayer(player.name)!!.uniqueId to commandNum
-                game.paintedPerson += Bukkit.getPlayer(player.name)!!.uniqueId to 0
+        // используем команды из teams внутри GameInstance
+        instance.teams.forEachIndexed { teamIndex, teamPlayers ->
+            teamPlayers.forEach { player ->
+                val bukkitPlayer = Bukkit.getPlayer(player.name) ?: return@forEach
+                playerGame[bukkitPlayer.uniqueId] = game
+                game.commands[bukkitPlayer.uniqueId] = teamIndex
+                game.paintedPerson[bukkitPlayer.uniqueId] = 0
             }
-            commandNum += 1
         }
+
         game.startGame()
     }
 
+
     fun deleteGame(worldName: String, game: Game) {
-        val multiverseCore: MultiverseCore =
-            Bukkit.getServer().pluginManager.getPlugin("Multiverse-Core") as MultiverseCore
+        val multiverseCore =
+            Bukkit.getPluginManager().getPlugin("Multiverse-Core") as MultiverseCore
+
         multiverseCore.mvWorldManager.deleteWorld(worldName)
-        File(Bukkit.getWorldContainer(), worldName).exists()
-        playerGame.entries.removeAll { entry ->
-            entry.value == game
-        }
+        File(Bukkit.getWorldContainer(), worldName).deleteRecursively()
+
+        playerGame.entries.removeAll { it.value == game }
         arenas.remove(worldName)
     }
 }
