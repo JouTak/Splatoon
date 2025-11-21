0105406478d80552a29681c06a5099311176a9a7
Saldaw <yolshin07@bk.ru>
2025-11-21 10:58:49 +0300

    Поидее теперь только правая кнопка стреляет

--- src/main/kotlin/ru/joutak/splatoon/listeners/PlayerUseListener.kt
+++ src/main/kotlin/ru/joutak/splatoon/listeners/PlayerUseListener.kt
@@ -11,41 +11,34 @@ import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.plugin.Plugin
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.NamespacedKey
+import org.bukkit.event.block.Action
 import org.bukkit.potion.PotionEffectType
 
 class PlayerUseListener(val plugin: Plugin) : Listener {
     @EventHandler
     fun playerUseItemEvent(event: PlayerInteractEvent) {
         val player = event.player
+        val action = event.action
         if (player.hasPotionEffect(PotionEffectType.INVISIBILITY)) return
-        if (player.inventory.itemInMainHand.type == Material.GOLDEN_SHOVEL && player.inventory.itemInMainHand.itemMeta.persistentDataContainer.has(
-                NamespacedKey(plugin, "splatGun"),
-                PersistentDataType.BOOLEAN
+        if ((action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) && player.inventory.itemInMainHand.type == Material.GOLDEN_SHOVEL && player.inventory.itemInMainHand.itemMeta.persistentDataContainer.has(
+                NamespacedKey(plugin, "splatGun"), PersistentDataType.BOOLEAN
             )
-        )
-            player.world.spawnEntity(
-                Location(
-                    player.world,
-                    player.eyeLocation.x,
-                    player.eyeLocation.y - 0.1,
-                    player.eyeLocation.z
-                ).add(player.location.direction), EntityType.SNOWBALL
-            ).apply {
-                velocity = player.location.direction.multiply(1.4)
-                setMetadata("paintKey", FixedMetadataValue(plugin, 1))
-                setMetadata("shooter", FixedMetadataValue(plugin, player.name))
-            }
+        ) player.world.spawnEntity(
+            Location(
+                player.world, player.eyeLocation.x, player.eyeLocation.y - 0.1, player.eyeLocation.z
+            ).add(player.location.direction), EntityType.SNOWBALL
+        ).apply {
+            velocity = player.location.direction.multiply(1.4)
+            setMetadata("paintKey", FixedMetadataValue(plugin, 1))
+            setMetadata("shooter", FixedMetadataValue(plugin, player.name))
+        }
         if (player.inventory.itemInMainHand.type == Material.GOLDEN_AXE && player.inventory.itemInMainHand.itemMeta.persistentDataContainer.has(
-                NamespacedKey(plugin, "splatBomb"),
-                PersistentDataType.BOOLEAN
+                NamespacedKey(plugin, "splatBomb"), PersistentDataType.BOOLEAN
             )
         ) {
             player.world.spawnEntity(
                 Location(
-                    player.world,
-                    player.eyeLocation.x,
-                    player.eyeLocation.y - 0.1,
-                    player.eyeLocation.z
+                    player.world, player.eyeLocation.x, player.eyeLocation.y - 0.1, player.eyeLocation.z
                 ).add(player.location.direction), EntityType.SNOWBALL
             ).apply {
                 customName(Component.text("Bomb"))
