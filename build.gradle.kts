d69e70c36374c94d35471c82ace391acf6228235
enderdissa <dberman@itmo.ru>
2026-01-06 19:31:58 +0300

    Fixed mirrors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
+val group: String by project
+val version: String by project
+val repo: String by project
+
+project.group = group
+project.version = version
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.shadow)
+}
+
 repositories {
     maven("https://maven-central.storage-download.googleapis.com/maven2/") {
         name = "maven-central-mirror"
@@ -13,3 +25,73 @@ repositories {
     maven("https://jitpack.io") { name = "jitpack" }
     maven("https://maven.joutak.ru/snapshots")
 }
+
+dependencies {
+    compileOnly(libs.kotlin)
+    compileOnly(libs.paper)
+    compileOnly("com.onarandombox.multiversecore:multiverse-core:4.3.14")
+
+    implementation("ru.joutak:minigamesapi:1.0.0-57")
+}
+
+kotlin {
+    jvmToolchain(
+        libs.versions.jdk
+            .get()
+            .toInt()
+    )
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.jar {
+    enabled = false
+}
+
+tasks.processResources {
+    val minecraftVersion =
+        libs.versions.paper
+            .get()
+            .substringBefore("-")
+
+    val commitHash = project.findProperty("commitHash") as String?
+
+    val website =
+        if (repo.isBlank()) {
+            "https://joutak.ru"
+        } else {
+            if (commitHash.isNullOrBlank()) repo else "$repo/tree/$commitHash"
+        }
+
+    val props =
+        mapOf(
+            "NAME" to project.name,
+            "VERSION" to project.version,
+            "MINECRAFT_VERSION" to minecraftVersion,
+            "KOTLIN_VERSION" to libs.versions.kotlin.get(),
+            "WEBSITE" to website
+        )
+
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks.shadowJar {
+    archiveFileName.set("${project.name}-${project.version}.jar")
+
+    if (System.getenv("TEST_PLUGIN_BUILD") != null) {
+        val serverPath = System.getenv("SERVER_PATH")
+        if (serverPath != null) {
+            destinationDirectory.set(file("$serverPath/plugins"))
+        } else {
+            logger.warn("SERVER_PATH property is not set!")
+        }
+    }
+}
+// Уберите лишнюю закрывающую скобку здесь
