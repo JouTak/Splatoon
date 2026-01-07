60479242bb2dcf0528ce0ad46b2227fb638caf40
enderdissa <dberman@itmo.ru>
2026-01-08 01:21:10 +0300

    updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     compileOnly(libs.paper)
     compileOnly("com.onarandombox.multiversecore:multiverse-core:4.3.14")
 
-    implementation("ru.joutak:minigamesapi:1.0.0-57")
+    implementation("ru.joutak:minigamesapi:1.0.0-61")
 }
 
 kotlin {
